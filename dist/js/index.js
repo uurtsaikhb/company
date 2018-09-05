cf3b58234410f14748f8dd3bf4d8f1ad338afb27
Uurtsaikh Batbileg <uurtsaikh.ba@gmail.com>
2018-09-05 01:35:41 -0700

    responsive menu

--- dist/js/index.js
+++ dist/js/index.js
@@ -9,4 +9,29 @@
             header.classList.remove('shadow');
         }
     };
+
+    document
+        .getElementById('menu-bar-wrapper')
+        .addEventListener('click', toggleNavigation);
+
+    function toggleNavigation() {
+        const navBarModal = document.getElementById('navbar-modal');
+        navBarModal.classList.add('open');
+        navBarModal.style.top = window.screenY - 22 + 'px';
+
+        document.body.classList.add('modal-open');
+    }
+
+    document
+        .getElementById('navbar-modal')
+        .addEventListener('click', function(e) {
+            if (e.target.tagName === 'LI') {
+                this.classList.remove('open');
+                document.body.classList.remove('modal-open');
+                window.location.hash = e.target.dataset.link;
+            } else if (e.target.tagName === 'SPAN') {
+                this.classList.remove('open');
+                document.body.classList.remove('modal-open');
+            }
+        });
 })();
